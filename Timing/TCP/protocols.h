9ce2e5ff2247f5d7d9575bd7a45f4618250a552e
DiegoH81 <hidal8909ins@gmail.com>
2026-06-15 21:57:42 -0500

    feat(Timing): add timing version

---
+++ Timing/TCP/protocols.h
@@ -0,0 +1,226 @@
+#ifndef PROTOCOLS_H
+#define PROTOCOLS_H
+
+#include <unistd.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+#include "json.hpp"
+
+// Auxiliar functions
+std::string get_number(int in_number, int in_bytes)
+{
+   std::string to_return(in_bytes, '0');
+
+   std::string num = std::to_string(in_number);
+
+
+   int eo_in_num = num.length() - 1;
+   int eo_return = in_bytes - 1;
+
+   for (int i = 0; i < num.length(); i++)
+   {
+      to_return[eo_return] = num[eo_in_num];
+      eo_return--; eo_in_num--;
+   }
+     
+   return to_return;
+}
+
+std::string read_string(int in_socket, int in_length)
+{
+   std::string to_return;
+   int n;
+   to_return.resize(in_length);
+
+   int total = 0;
+
+   while (total < in_length)
+   {
+      n = read(in_socket, &to_return[total], in_length - total);
+
+      if (n <= 0)
+         break;
+         
+      total += n;
+   }
+
+   return to_return;
+}
+
+int read_number(int in_socket, int in_length)
+{
+   std::string num = read_string(in_socket, in_length);
+
+   return std::stoi(num);
+}
+
+std::string read_binary_file(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+
+    if (!file.is_open())
+        return ""; 
+
+    file.seekg(0, std::ios::end);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::string buffer;
+    buffer.resize(size);
+
+
+    if (file.read(&buffer[0], size))
+        return buffer;
+
+    return "";
+}
+
+bool write_binary_file(const std::string& filename, const std::string& data)
+{
+    std::ofstream file(filename, std::ios::binary | std::ios::out | std::ios::trunc);
+
+    if (!file.is_open())
+        return false;
+
+    if (file.write(data.data(), data.size()))
+        return true;
+
+    return false;
+}
+
+namespace prt_send
+{
+    void ping(int in_socket)
+    {
+        write(in_socket, "P", 1);
+    }
+
+    void pong(int in_socket)
+    {
+        write(in_socket, "G", 1);
+    }
+   // Client to SV
+   void login(std::string& in_nickname, int in_socket)
+   {
+      int in_bytes = 4;
+
+      if (in_nickname.length() >= 1000)
+         in_nickname.resize(999);
+
+      std::string to_send = "L" + get_number(in_nickname.length(), in_bytes) + in_nickname;
+      write(in_socket, to_send.c_str(), to_send.length());
+   }
+   
+   void logout(int in_socket)
+   {
+      write(in_socket, "O", 1);
+   }
+   
+   void file(std::string file_name, std::string file, std::string dest, int in_socket)
+   {
+      if(file.size() >= 100000)
+         file.resize(99999);
+
+      if(file_name.size() >= 100000)
+         file_name.resize(99999);
+
+      if(dest.size() >= 100000)
+         dest.resize(99999);
+
+      int bytes = 5;
+
+      std::string to_send = "F" + get_number(file.length(), bytes) + file + 
+                                  get_number(file_name.length(), bytes) + file_name +
+                                  get_number(dest.length(), bytes) + dest;
+      
+      write(in_socket, to_send.c_str(), to_send.length());
+   }
+
+   // SV to client
+
+   void error(const std::string& in_msg, int in_socket)
+   {
+      int in_bytes = 5;
+
+      auto msg = in_msg;
+      if (msg.length() >= 100000)
+         msg.resize(99999);
+
+      std::string to_send = "E" + get_number(msg.length(), in_bytes) + msg;
+      write(in_socket, to_send.c_str(), to_send.length());
+   }
+
+   void k_response(int in_socket)
+   {
+      write(in_socket, "K", 1);
+   }
+
+   
+   void file_response(std::string file_name, std::string file, std::string src, int in_socket)
+   {
+      if(file.size() >= 100000)
+         file.resize(99999);
+
+      if(file_name.size() >= 100000)
+         file_name.resize(99999);
+
+      if(src.size() >= 100000)
+         src.resize(99999);
+
+      int bytes = 5;
+
+      std::string to_send = "f" + get_number(file.length(), bytes) + file + 
+                                  get_number(file_name.length(), bytes) + file_name +
+                                  get_number(src.length(), bytes) + src;
+      
+      write(in_socket, to_send.c_str(), to_send.length());
+   }
+   
+}
+
+namespace prt_recv
+{
+   // Client to SV
+   std::string login(int in_socket)
+   {
+      int L = read_number(in_socket, 4);
+      return read_string(in_socket, L);
+   }
+
+   std::string broadcast(int in_socket)
+   {
+      int L = read_number(in_socket, 7);
+      return read_string(in_socket, L);
+   }
+
+   // Server to client
+
+   bool k_response(int in_socket)
+   {
+      if (read_string(in_socket, 1) == "K")
+         return true;
+      return false;
+   }
+
+   std::string error(int in_socket)
+   {
+      int L = read_number(in_socket, 5);
+      return read_string(in_socket, L);
+   }
+
+   void file_response(std::string& file_name, std::string& file, std::string& dest, int in_socket)
+   {
+      int L = read_number(in_socket, 5);
+      file = read_string(in_socket, L);
+      L = read_number(in_socket, 5);
+      file_name = read_string(in_socket, L);
+      L = read_number(in_socket, 5);
+      dest = read_string(in_socket, L);
+   }
+
+}
+
+#endif
---
+++ Timing/UDP/client.cpp
@@ -0,0 +1,191 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <thread>
+
+//#include "protocols_TCP.h"
+#include "UDP_class.h"
+
+bool connected = false;
+
+UDPClient client;
+std::vector<std::string> all_msgs;
+
+void reader_UDP(int in_socket)
+{
+    struct sockaddr_in sender_addr;
+    
+    while(connected)
+    {
+        std::string msg = client.recv_fragment(in_socket, sender_addr);
+        
+        if (msg.empty())
+            continue;
+
+        char opt = msg.front();
+        msg = msg.substr(1);
+
+        switch(opt)
+        {
+        case 'F':
+        {
+            std::string ori, dest, file_name, file;
+            client.parse_file(msg, ori, dest, file_name, file);
+
+            write_binary_file("new_" + file_name, file);
+            all_msgs.push_back("SERVER: File received");
+            break;
+        }
+        case 'E':
+        {
+            std::string error_msg;
+            client.parse_error(msg, error_msg);
+
+            all_msgs.push_back("ERROR: " + error_msg);
+            break;
+        }
+        case 'G':
+        {
+            auto t_end = std::chrono::high_resolution_clock::now();
+            double ping_ms = std::chrono::duration<double, std::milli>(t_end - client.ping_start).count();
+            all_msgs.push_back("PING: " + std::to_string(ping_ms) + " ms");
+            break;
+        }
+        default:
+        {
+            std::string un_command = "Unrecognized command! (" + std::string(1, opt) + ")";
+            all_msgs.push_back(un_command);
+            break;
+        }
+        }
+    }
+}
+
+
+int main(void)
+{
+    struct sockaddr_in stSockAddr;
+    int Res;
+    int SocketFD;
+
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(45000);
+    stSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    
+    std::string nick, t_buffer;
+
+    while(true)
+    {
+        //system("clear");
+
+
+        std::cout << "======= Chat log =======\n";
+        for (auto &msg: all_msgs)
+            std::cout << msg << "\n";
+        std::cout << "\n\n";
+        
+        std::cout << "======= MENU CLIENT =======\n";
+        if (connected)
+            std::cout << "Nick: " << nick << "\n"; 
+        std::cout << "Options:\n\n";
+
+        if (!connected)
+            std::cout << "1. Login\n";
+
+        if (connected)
+        {
+            std::cout << "2. Logout\n";
+            std::cout << "3. File\n";
+            std::cout << "4. Ping\n";
+            std::cout << "5. Update Chat\n";
+        }
+            
+        std::string buffer;
+        std::cout << "Enter option: ";
+        std::getline(std::cin, buffer);
+
+        if (buffer == "1")
+        {
+            if (connected)
+                continue;
+
+            SocketFD = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+            do
+            {
+                std::cout << "Enter nickname: ";
+                std::getline(std::cin, nick);
+                
+                auto t_rtt_start = std::chrono::high_resolution_clock::now();
+
+                client.send_login(nick, SocketFD, stSockAddr);
+
+                auto a_n_e = client.ack_or_error(SocketFD, stSockAddr);
+                if (a_n_e == 'K')
+                {
+                    std::thread(reader_UDP, SocketFD).detach();
+                    connected = true;
+
+                    auto t_rtt_end = std::chrono::high_resolution_clock::now();
+                    double rtt_ms = std::chrono::duration<double, std::milli>(t_rtt_end - t_rtt_start).count();
+                    std::cout << "RTT: " << rtt_ms << " ms\n";
+                }
+                else
+                    all_msgs.push_back("Server: Username already taken!");
+                
+            } while (!connected);
+        }
+        else if (buffer == "2")
+        {
+            if (connected)
+            {
+                client.send_logout(nick, SocketFD, stSockAddr);
+                connected = false;
+            }
+            else
+                std::cout << "Please login first!\n";
+        }
+        else if (buffer == "3")
+        {
+            if (connected)
+            {
+                std::string file_name;
+                std::cout << "Enter file name: ";
+                std::getline(std::cin, file_name);
+        
+                std::string dst;
+                std::cout << "Enter destinatary: ";
+                std::getline(std::cin, dst);
+
+                std::string file = read_binary_file(file_name);
+                if (file.empty())
+                {
+                    all_msgs.push_back(std::string("ERROR: Empty or unexistent file"));
+                    continue;
+                }
+
+                client.send_file(file_name, file, dst, nick, SocketFD, stSockAddr);
+            }
+            else
+                std::cout << "Please login first!\n";
+        }
+        else if (buffer == "4")
+        {
+            client.ping(SocketFD, stSockAddr);
+            
+        }
+    }
+
+    close(SocketFD);
+    return 0;
+}
